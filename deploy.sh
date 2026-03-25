ece1b589220d5b365fae7df32de54f6e30ef392c
Brandon Dorsey <brandontdr@gmail.com>
2026-03-24 21:29:22 -0400

    First commit to main

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/user/bin/env sh
+
+set -e
+
+npm run build
+cd dist
+git init
+git add -A
+git commit -m 'New Deployment'
+git push -f git@github.com:brandontdr/vue-pages.git master:gh-pages
+cd -
