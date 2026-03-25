26d3edd98df60d48e0e7a7290f142d08873954d0
Brandon Dorsey <brandontdr@gmail.com>
2026-03-24 21:29:22 -0400

    First commit to main

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+
+    <h1>Random Quote Generator</h1>
+
+    <p v-if="loading">Loading...</p>
+    <p v-else>"{{ quote }}"</p>
+    <p class="author">— {{ author }}</p>
+
+    <button @click="getQuote">Get New Quote</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data () {
+    return {
+      quote: '',
+      author: '',
+      loading: false
+    }
+  },
+  methods: {
+    async getQuote () {
+      this.loading = true
+
+      try {
+        const response = await axios.get('https://quotes15.p.rapidapi.com/quotes/random/?language_code=en', {
+          headers: {
+            'x-rapidapi-host': 'quotes15.p.rapidapi.com',
+            'x-rapidapi-key': 'f5013b25e6mshe19bb6f363a5474p1267a7jsn51f1b9b4703f'
+          }
+        })
+
+        this.quote = response.data.content;
+        this.author = response.data.author;
+
+      } catch (error) {
+        console.error('Error fetching quote: ', error);
+        this.quote = 'Oops! Something went wrong.'
+        this.author = ''
+      } finally {
+        this.loading = false
+      }
+
+    }
+  },
+
+  mounted() {
+    this.getQuote();
+  },
+
 }
 </script>
 
